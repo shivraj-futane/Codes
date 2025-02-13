03ba5b82b1cc68288edb5975131d163d8dc8aca1
Shivraj <mr.shivrajfutane@gmail.com>
2025-02-13 13:47:21 +0530

    13-02-2025 JAVA

---
+++ Java Int/DSA/src/stringoperations.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class stringoperations {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter First String");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
